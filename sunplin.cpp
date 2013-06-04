893d16a2b395f3d8c5475aff18863e15481de1cf
Wellington Martins <martinswellington@gmail.com>
2013-06-04 19:17:19 -0300

    commit

--- sunplin.cpp
+++ sunplin.cpp
@@ -1,3 +1,23 @@
+/*********************************************************************
+11	
+12	 Copyright (C) 2013-2016 by Welton Cardoso
+13	
+14	 This program is free software; you can redistribute it and/or modify
+15	 it under the terms of the GNU General Public License as published by
+16	 the Free Software Foundation; either version 2 of the License, or
+17	 (at your option) any later version.
+18	
+19	 This program is distributed in the hope that it will be useful,
+20	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+21	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+22	 GNU General Public License for more details.
+23	
+24	 You should have received a copy of the GNU General Public License
+25	 along with this program; if not, write to the Free Software
+26	 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+27	
+28	 ********************************************************************/
+
 #include <map>
 #include <cmath>
 #include <ctime>
